c711aefc9e19e421a353c96dbfaa8ae6770fd6a8
Ella <ellalearnscopy@gmail.com>
2023-09-26 04:13:16 +0100

    feat: bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an integer array with bubble sort in ascending order
+ * @array: the integer array
+ * @size: the size of the array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+    int was_sort = 1;
+    size_t len = size;
+
+    if (len > 1)
+    {
+        while (array && was_sort > 0)
+        {
+            int ind = 1;
+            int highest_int = 0;
+            was_sort = 0;
+
+            while (ind < len)
+            {
+                int temp = 0;
+
+                if (array[ind - 1] > array[ind])
+                {
+                    temp = array[ind - 1];
+                    array[ind - 1] = array[ind];
+                    array[ind] = temp;
+
+                    was_sort += 1;
+                    print_array(array, size)
+                }
+
+                if (temp > highest_int)
+                    highest_int = temp;
+
+                ind += 1;
+            }
+
+            if (array[ind - 1] == highest_int)
+                len -= 1;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+## Sorting Algorithms
+
+Contains four sorting algorithms
+- Bubble Sort
+- Insertion Sort
+- Selection Sort
+- Quick Sort
+
+Enjoy
