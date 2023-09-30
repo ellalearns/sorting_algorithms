20483ddb93fb880d7dab5d4090633ba0b9bbc726
Ella <ellalearnscopy@gmail.com>
2023-09-30 05:45:39 +0100

    feat: selection_sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort ints in asce order with selection sort
+ * @array: the list of ints
+ * @size: size of int
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t cIndex = 0;
+    size_t mIndex = 0;
+    size_t listlen = size;
+
+    if (listlen > 1)
+    {
+        while (array && cIndex < listlen)
+        {
+            size_t tmp1 = array[cIndex];
+            size_t tmp2 = 0;
+            mIndex = cIndex + 1;
+
+            while (mIndex < listlen)
+            {
+                if (array[mIndex] < tmp1)
+                {
+                    tmp2 = array[mIndex];
+                    array[mIndex] = tmp1;
+                    tmp1 = tmp2;
+                }
+                mIndex++;
+            }
+
+            array[cIndex] = tmp1;
+            cIndex++;
+        }
+    }
+}
