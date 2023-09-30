37e919192fb85f40b8a30bbe3ab18fccd647deef
Ella <ellalearnscopy@gmail.com>
2023-09-30 05:50:24 +0100

    feat: selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,37 +1,38 @@
 #include "sort.h"
 
 /**
- * selection_sort - sort ints in asce order with selection sort
- * @array: the list of ints
- * @size: size of int
- */
+* selection_sort - sort ints in asce order with selection sort
+* @array: the list of ints
+* @size: size of int
+*/
 void selection_sort(int *array, size_t size)
 {
-    size_t cIndex = 0;
-    size_t mIndex = 0;
-    size_t listlen = size;
+size_t cIndex = 0;
+size_t mIndex = 0;
+size_t listlen = size;
 
-    if (listlen > 1)
-    {
-        while (array && cIndex < listlen)
-        {
-            int tmp1 = array[cIndex];
-            int tmp2 = 0;
-            mIndex = cIndex + 1;
+if (listlen > 1)
+{
+while (array && cIndex < listlen)
+{
+int tmp1 = array[cIndex];
+int tmp2 = 0;
 
-            while (mIndex < listlen)
-            {
-                if (array[mIndex] < tmp1)
-                {
-                    tmp2 = array[mIndex];
-                    array[mIndex] = tmp1;
-                    tmp1 = tmp2;
-                }
-                mIndex++;
-            }
+mIndex = cIndex + 1;
 
-            array[cIndex] = tmp1;
-            cIndex++;
-        }
-    }
+while (mIndex < listlen)
+{
+if (array[mIndex] < tmp1)
+{
+tmp2 = array[mIndex];
+array[mIndex] = tmp1;
+tmp1 = tmp2;
+}
+mIndex++;
+}
+
+array[cIndex] = tmp1;
+cIndex++;
+}
+}
 }
